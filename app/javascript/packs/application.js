31133a895ab75c4916e5f45a4d7d6652e124b4a3
Eric <djbartos93@gmail.com>
2021-06-09 16:51:09 -0500

    !!!!

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "chota/src/chota.css";
+import "chota/dst/chota.css";
 
 Rails.start()
 Turbolinks.start()
