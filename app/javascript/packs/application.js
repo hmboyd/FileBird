2c940b7d1c6230923ef49eb59a41d0bde49100f6
Eric <djbartos93@gmail.com>
2021-06-09 16:35:56 -0500

    its not dark mode but maybe it will work???

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "chota/src/chota.css";
 
 Rails.start()
 Turbolinks.start()
