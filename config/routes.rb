3dd58dec589520a5a92e815fc2048cf6fabb7065
Eric <djbartos93@gmail.com>
2021-06-09 13:50:41 -0500

    make it kinda pretty and set timezone!

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   mount Sidekiq::Web, at: '/sidekiq'
+
+  root to: 'downloaders#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
